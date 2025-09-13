2d998e35136d0e87fc5e5b2abfff5bfdcaaf2890
Preston <vazpreston250@gmail.com>
2025-09-13 13:05:03 +0530

    Update java.js

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-const CLIENT_ID = "821c2e004b094b928bdf7d4ab553e034";
+const CLIENT_ID = "";
 const REDIRECT_URI = "https://aaes250.github.io/test-app/"; // must match Spotify dashboard
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
@@ -161,3 +161,4 @@ logoutBtn.addEventListener("click", logout);
 
 // ===== INIT =====
 main();
+
